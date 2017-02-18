cdabea774e6aec228e214ced425c30cce8e478e3
jaworek <jaworek3211@gmail.com>
2017-02-18 16:42:15 +0000

    Betting, value check and horse running

--- game.js
+++ game.js
@@ -1,101 +1,59 @@
-var player;
-var upPressed = false;
-var downPressed = false;
-var leftPressed = false;
-var rightPressed = false;
+var funds = 100;
+var start;
+var horse;
 
-function keyup(event) {
-	if (event.keyCode == 37) {		
-		leftPressed = false;
-		if (!leftPressed && !rightPressed && !upPressed && !downPressed) {
-			player.className = 'character standLeft';
-		}
-	}
-	if (event.keyCode == 39) {
-		rightPressed = false;
-		if (!leftPressed && !rightPressed && !upPressed && !downPressed) {
-			player.className = 'character standRight';
-		}
-	}
-	if (event.keyCode == 38) {
-		upPressed = false;
-		if (!leftPressed && !rightPressed && !upPressed && !downPressed) {
-			player.className = 'character standUp';
-		}
-	}
-	if (event.keyCode == 40) {
-		downPressed = false;
-		if (!leftPressed && !rightPressed && !upPressed && !downPressed) {
-			player.className = 'character standDown';
-		}
-	}	
+function startButton()
+{
+    console.log('Start button was pressed');
+    var betHorse = document.getElementById('bethorse').value;
+    var betAmount = document.getElementById('amount').value;
+    if (betAmount > funds)
+    {
+        console.log('Insufficient funds');
+    }
+    else if (betAmount == '' || betAmount == 0)
+    {
+        console.log('You need to specify the amount');
+    }
+    else
+    {
+        console.log(betHorse);
+        console.log(betAmount);
+        funds -= betAmount;
+        document.getElementById('funds').innerHTML = funds;
+        startRace();
+    }
 }
 
-
-function move() {
-	var positionLeft = player.offsetLeft;
-	var positionTop = player.offsetTop;
-	if (downPressed) {
-		player.style.top = positionTop+1 + 'px';
-		player.className = 'character walkDown';
-	}
-	if (upPressed) {
-		player.style.top = positionTop-1 + 'px';
-		player.className = 'character walkUp';
-	}
-	if (leftPressed) {
-		player.style.left = positionLeft-1 + 'px';
-		player.className = 'character walkLeft';
-	}
-	if (rightPressed) {
-		player.style.left = positionLeft+1 + 'px';
-		player.className = 'character walkRight';
-	}
-
+function startRace()
+{
+    console.log('Race started');
+    start.disabled = true;
+    horse = document.getElementsByClassName('horse');
+    for (var i = 0; i < horse.length; i++)
+    {
+        horse[i].className = 'horse runRight';
+        //setInterval(function(){horseRun(i);}, 10);
+        //horseRun(i);
+    }
+    setInterval(function(){horseRun(0);}, 10);
+    setInterval(function(){horseRun(1);}, 7);
+    setInterval(function(){horseRun(2);}, 8);
+    setInterval(function(){horseRun(3);}, 9);
 }
 
-
-function keydown(event) {
-	if (event.keyCode == 37) {		
-		leftPressed = true;
-	}
-	if (event.keyCode == 39) {
-		rightPressed = true;
-	}
-	if (event.keyCode == 38) {
-		upPressed = true;
-	}
-	if (event.keyCode == 40) {
-		downPressed = true;
-	}	
+function horseRun(i)
+{
+    var positionLeft = horse[i].offsetLeft;
+    horse[i].style.left = positionLeft + 1 + 'px';
 }
 
+function myLoadFunction()
+{
+    start = document.getElementById('start');
+    start.addEventListener('click', startButton);
 
-function setHead() {
-	player.getElementsByClassName('head')[0].style.backgroundImage = 'url(images/' + this.id + '.png)';
-}
-
-function setBody() {
-	player.getElementsByClassName('body')[0].style.backgroundImage = 'url(images/' + this.id + '.png)';
-}
-
-function myLoadFunction() {
-	player = document.getElementById('player');
-
-	var heads = document.getElementsByClassName('heads')[0].getElementsByTagName('li');
-	for (var i = 0; i < heads.length; i++) {
-		heads[i].addEventListener('click', setHead);
-	}
-	
-	var bodies = document.getElementsByClassName('bodies')[0].getElementsByTagName('li');
-	
-	for (var i = 0; i < heads.length; i++) {
-		bodies[i].addEventListener('click', setBody);
-	}
-
-	timeout = setInterval(move, 10);
-	document.addEventListener('keydown', keydown);
-	document.addEventListener('keyup', keyup);
+    document.getElementById('funds').innerHTML = funds;
 }
 
 
